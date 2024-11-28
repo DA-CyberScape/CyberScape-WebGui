4d8dde27925c1993ece35bfbd30a415fa469b6fc
Schlindus <frulin6@gmail.com>
2024-11-28 18:21:25 +0100

    updated DataSources

--- src/lib/sources.ts
+++ src/lib/sources.ts
@@ -4,26 +4,38 @@ let sources: string =
 	'  {\n' +
 	'    "snmpPolls": [\n' +
 	'      {\n' +
-	'        "name": "FirewallSNMP",\n' +
-	'        "id": 123,\n' +
-	'        "ip": "192.168.10.254",\n' +
-	'        "hostname": "FW-1123123123123123123123123",\n' +
+	'        "name": "FG-SNMP",\n' +
+	'        "id": 123123,\n' +
+	'        "ip": "10.0.1.254",\n' +
+	'        "hostname": "FG-SNMP",\n' +
 	'        "oids": [\n' +
 	'          {\n' +
-	'            "oid": "1.3.6.1.4.1.9.2.1.56.0",\n' +
-	'            "name": "CPU Load"\n' +
+	'            "oid": "1.3.6.1.4.1.12356.101.4.1.3.0",\n' +
+	'            "name": "CPU LOAD"\n' +
 	'          },\n' +
 	'          {\n' +
-	'            "oid": "1.3.6.1.4.1.9.2.1.8.0",\n' +
-	'            "name": "Memory Usage"\n' +
+	'            "oid": "1.3.6.1.4.1.12356.101.4.1.4.0",\n' +
+	'            "name": "MEMORY USAGE"\n' +
 	'          },\n' +
 	'          {\n' +
 	'            "oid": "1.3.6.1.2.1.1.3.0",\n' +
-	'            "name": "Uptime"\n' +
+	'            "name": "UPTIME"\n' +
 	'          },\n' +
 	'          {\n' +
-	'            "oid": "1.3.6.1.2.1.6.9.0",\n' +
-	'            "name": "TCP Connections"\n' +
+	'            "oid": "1.3.6.1.4.1.12356.101.4.1.1.0",\n' +
+	'            "name": "VERSION"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": "1.3.6.1.4.1.12356.101.7.2.2.1.1",\n' +
+	'            "name": "VLANS"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": "1.3.6.1.4.1.12356.101.3.2.1.1.7.1",\n' +
+	'            "name": "SESSION COUNT"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": "1.3.6.1.2.1.47.1.1.1.1.14",\n' +
+	'            "name": "INTERFACES"\n' +
 	'          }\n' +
 	'        ],\n' +
 	'        "user": "MY-USER",\n' +
@@ -34,29 +46,50 @@ let sources: string =
 	'        "port": 161\n' +
 	'      },\n' +
 	'      {\n' +
-	'        "name": "SwitchSNMP",\n' +
-	'        "id": 123123,\n' +
-	'        "ip": "192.168.10.20",\n' +
-	'        "hostname": "Switch-1",\n' +
+	'        "name": "Switch-SNMP",\n' +
+	'        "id": 293485234,\n' +
+	'        "ip": "192.168.50.1",\n' +
+	'        "hostname": "Switch-SNMP",\n' +
 	'        "oids": [\n' +
 	'          {\n' +
-	'            "oid": "1.3.6.1.4.1.9.2.1.56.0",\n' +
-	'            "name": "CPU Load"\n' +
+	'            "oid": ".1.3.6.1.2.1.1.5.0",\n' +
+	'            "name": "HOSTNAME"\n' +
 	'          },\n' +
 	'          {\n' +
-	'            "oid": "1.3.6.1.4.1.9.2.1.8.0",\n' +
-	'            "name": "Memory Usage"\n' +
+	'            "oid": ".1.3.6.1.4.1.9.2.1.57.0",\n' +
+	'            "name": "CPU LOAD"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": ".1.3.6.1.4.1.9.2.1.9.0",\n' +
+	'            "name": "MEMORY USAGE"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": ".1.3.6.1.4.1.9.2.1.73.0",\n' +
+	'            "name": "VERSION"\n' +
 	'          },\n' +
 	'          {\n' +
 	'            "oid": "1.3.6.1.2.1.1.3.0",\n' +
-	'            "name": "Uptime"\n' +
+	'            "name": "UPTIME"\n' +
+	'          },\n' +
+	'          \n' +
+	'          {\n' +
+	'            "oid": ".1.3.6.1.2.1.2.2.1.2",\n' +
+	'            "name": "INTERFACES"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": ".1.3.6.1.2.1.2.2.1.16",\n' +
+	'            "name": "OUTBOUND TRAFFIC INTERFACES"\n' +
+	'          },\n' +
+	'          {\n' +
+	'            "oid": ".1.3.6.1.2.1.2.2.1.10",\n' +
+	'            "name": "INBOUND TRAFFIC INTERFACES"\n' +
 	'          }\n' +
 	'        ],\n' +
-	'        "user": "MY-USER",\n' +
+	'        "user": "my-user",\n' +
 	'        "authentication": "SHA1",\n' +
 	'        "encryption": "aes 128",\n' +
-	'        "authpass": "MyAuthPass",\n' +
-	'        "privpass": "MyPrivPass",\n' +
+	'        "authpass": "junioradmin",\n' +
+	'        "privpass": "junioradmin",\n' +
 	'        "port": 161\n' +
 	'      }\n' +
 	'    ]\n' +
@@ -66,60 +99,10 @@ let sources: string =
 	'      {\n' +
 	'        "name": "Netflow1",\n' +
 	'        "id": 12312,\n' +
-	'        "folderLocation": "/path/to/folder",\n' +
-	'        "nfdumpBinaryLocation": "/usr/local/bin/nfdump",\n' +
-	'        "nfcapdBinaryLocation": "/usr/local/bin/nfcapd",\n' +
+	'        "folderLocation": "/home/cyberscape_admin/Netflow_example_files",\n' +
+	'        "nfdumpBinaryLocation": "/usr/bin/nfdump",\n' +
+	'        "nfcapdBinaryLocation": "/usr/bin/nfcapd",\n' +
 	'        "port": 2055\n' +
-	'      },\n' +
-	'      {\n' +
-	'        "name": "Netflow2",\n' +
-	'        "id": 12312123,\n' +
-	'        "folderLocation": "/path/to/123123",\n' +
-	'        "nfdumpBinaryLocation": "/usr/local/bin/nfdump123123",\n' +
-	'        "nfcapdBinaryLocation": "/usr/local/bin/nfcapd1231231",\n' +
-	'        "port": 21231231231\n' +
-	'      }\n' +
-	'    ]\n' +
-	'  },\n' +
-	'  {\n' +
-	'    "PRTGReceiver": [\n' +
-	'      {\n' +
-	'        "name": "PRTG1",\n' +
-	'        "id": 12312123,\n' +
-	'        "PRTGUrl": "http://192.168.10.0",\n' +
-	'        "apiToken": "abcdefghijklmneopqrstuvwxyz"\n' +
-	'      },\n' +
-	'      {\n' +
-	'        "name": "PRTG2",\n' +
-	'        "id": 3452,\n' +
-	'        "PRTGUrl": "http://192.168.120.0",\n' +
-	'        "apiToken": "abcdefghijklmneopqr12312312312312312stuvwxyz"\n' +
-	'      }\n' +
-	'    ]\n' +
-	'  },\n' +
-	'  {\n' +
-	'    "snmpTrapReceiver": [\n' +
-	'      {\n' +
-	'        "name": "Trap1",\n' +
-	'        "id": 12312312,\n' +
-	'        "port": 162,\n' +
-	'        "version": "SNMPv3",\n' +
-	'        "username": "snmpv3user",\n' +
-	'        "authProtocol": "SHA",\n' +
-	'        "authPassword": "authPassword123",\n' +
-	'        "privacyProtocol": "AES",\n' +
-	'        "privacyPassword": "privPassword123"\n' +
-	'      },\n' +
-	'      {\n' +
-	'        "name": "Trap2",\n' +
-	'        "id": 5675,\n' +
-	'        "port": 162,\n' +
-	'        "version": "SNMPv2",\n' +
-	'        "username": "snmpv3user",\n' +
-	'        "authProtocol": "SHA",\n' +
-	'        "authPassword": "authPassword123",\n' +
-	'        "privacyProtocol": "AES",\n' +
-	'        "privacyPassword": "privPassword123"\n' +
 	'      }\n' +
 	'    ]\n' +
 	'  },\n' +
@@ -129,14 +112,17 @@ let sources: string =
 	'        "name": "Sys1",\n' +
 	'        "id": 56756,\n' +
 	'        "port": 514\n' +
-	'      },\n' +
-	'      {\n' +
-	'        "name": "Sys2",\n' +
-	'        "id": 12312,\n' +
-	'        "port": 12312\n' +
 	'      }\n' +
 	'    ]\n' +
-	'  },\n' +
+	'  },{\n' +
+	'  "snmpTrapReceiver": [\n' +
+	'    {\n' +
+	'      "name": "SNMP-Trap-Receiver",\n' +
+	'      "id": 123129,\n' +
+	'      "port": 162\n' +
+	'    }\n' +
+	'  ]\n' +
+	'},\n' +
 	'  {\n' +
 	'    "ScyllaDB": {\n' +
 	'      "port": 123,\n' +
@@ -162,11 +148,11 @@ const structure: string =
 	'      "user": "string",\n' +
 	'      "authentication": {\n' +
 	'        "type": "string",\n' +
-	'        "enum": ["SHA", "MD5"]\n' +
+	'        "enum": ["SHA", "SHA1", "MD5"]\n' +
 	'      },\n' +
 	'      "encryption": {\n' +
 	'        "type": "string",\n' +
-	'        "enum": ["AES", "DES", "3DES"]\n' +
+	'        "enum": ["AES", "AES128", "DES", "3DES"]\n' +
 	'      },\n' +
 	'      "authpass": "string",\n' +
 	'      "privpass": "string",\n' +
